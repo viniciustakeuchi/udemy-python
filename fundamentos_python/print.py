86efd927eabfff97f7b1bb0742b8bb0b710d3d3b
viniciustakeuchi <viniciustakeuchi1@gmail.com>
2026-01-12 20:43:27 -0300

    métodos em python

--- fundamentos_python/print.py
+++ fundamentos_python/print.py
@@ -7,3 +7,29 @@ fruta_favorita = input('Digite a sua fruta favorita: ')
 # print com f-string 
 print(f'Olá {nome}, daqui 10 anos você terá {idade + 10} de idade e sua fruta favorita é {fruta_favorita} eu imagino rs')
 
+mensagem = 'Olá meu nome é Vinícius'
+# Tudo minúsculo
+print(mensagem.lower())
+# Mostra o index
+print(mensagem.find('V'))
+# Muda a palavra
+print(mensagem.replace('Vinícius', 'Roberto'))
+nome = 'Ana'
+mensagem2 = f'    Ola, meu nome é {nome}'
+
+# Divide um string por lista de substrings 
+print(mensagem2.split())
+# Saída: ['Ola,', 'meu', 'nome', 'é', 'Ana']
+
+# Remove os espaços do início
+print(mensagem2.strip())
+
+
+
+
+
+
+
+'''
+Aqui eu posso escrever varias linhas de comentários
+'''
