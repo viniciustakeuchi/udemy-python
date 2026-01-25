d599fa3a74408e598e824d054dfcd85647ccd663
viniciustakeuchi <viniciustakeuchi1@gmail.com>
2026-01-25 20:03:18 -0300

    update

--- fundamentos_python/map.py
+++ fundamentos_python/map.py
@@ -3,10 +3,19 @@ Muito utilizado com Listas
 Aplicar uma função a um Iterable, por item. (list, tuple, dic etc.)
 '''
 
+
+
+# def multi(x):
+ #   return x * 2
+#lista2 = map(lambda x: x * 2, lista1)
 lista1 = [1, 2, 3, 4]
+print(list(map(lambda x: x * 2, lista1)))
 
-def multi(x):
-    return x * 2
+lista2 = [10, 20, 30, 40, 50]
+print(list(map(lambda x: x + 10, lista2)))
 
-lista2 = map(multi, lista1)
-print(list(lista2))
+lista3 = [2, 3, 4, 5]
+print(list(map(lambda x: x ** 2, lista3)))
+
+
+
