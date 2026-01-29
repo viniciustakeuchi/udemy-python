06732fe386e2fbb8b9db7aba7f12f77ceb708390
viniciustakeuchi <viniciustakeuchi1@gmail.com>
2026-01-29 19:49:45 -0300

    erro

---
+++ fundamentos_python/erros.py
@@ -0,0 +1,10 @@
+# Erros 
+# Excelentes para testes
+# Não realiza o 'stop' no programa
+# Mensagens customizadas quando encontra um erro
+
+try:
+    letras = ['a', 'b', 'c']
+    print(letras[3])
+except IndexError:
+    print('Index não existe')
